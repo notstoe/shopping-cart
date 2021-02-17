e7c4121883d97a22986cde8058aabb772a0d99eb
notstoe <gstonin95@gmail.com>
2021-02-17 19:31:23 +0000

    homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-	return <div>Home page</div>;
+	return (
+		<div className="homeContent">
+			<div className="quote">
+				<p>"Better three hours too soon than a minute too late."</p>
+				<span> – William Shakespeare</span>
+			</div>
+			<Link to="/shop" id="shopBtn">
+				VISIT THE SHOP
+			</Link>
+		</div>
+	);
 }
 
 export default Home;
