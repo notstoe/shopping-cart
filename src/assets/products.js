2aeed9655131a8ed5d66019899c34fc693e2ef3e
notstoe <gstonin95@gmail.com>
2021-03-09 20:10:53 +0000

    array with all imgs for easy access

---
+++ src/assets/products.js
@@ -0,0 +1,21 @@
+import backtothefuture from "./backtothefuture.png";
+import tangieWhite from "./concreteTangerineWhite.png";
+import honeycomb from "./honeycomb.png";
+import legendOfZelda from "./legendofZelda.png";
+import minimalist from "./minimalist.png";
+import steamPunk from "./steampunk.png";
+import trusttheprocess from "./trusttheprocess.png";
+import worldMap from "./worldMap.png";
+
+// Array containing all products
+
+export const products = [
+	{ img: backtothefuture, alt: "Back to the future clock", price: "39.99" },
+	{ img: tangieWhite, alt: "Tangerine White clock", price: "19.99" },
+	{ img: honeycomb, alt: "Honeycomb clock", price: "19.99" },
+	{ img: legendOfZelda, alt: "Legend of Zelda clock", price: "49.99" },
+	{ img: minimalist, alt: "Minimalist clock", price: "19.99" },
+	{ img: steamPunk, alt: "Steam punk clock", price: "19.99" },
+	{ img: trusttheprocess, alt: "Trust the process clock", price: "19.99" },
+	{ img: worldMap, alt: "World map clock", price: "29.99" },
+];
