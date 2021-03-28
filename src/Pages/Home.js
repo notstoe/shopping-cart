9e4a049effe2effd2eb44de9584301bff62aeef3
notstoe <gstonin95@gmail.com>
2021-03-28 22:33:16 +0100

    removed animation from this component and added to parent component App

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { motion } from "framer-motion";
 import styles from "../styles/Pages/Home.module.css";
 
 function Home() {
 	return (
-		<motion.div
-			className={styles.homeContent}
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ type: "tween", delay: 0.2 }}
-		>
+		<div className={styles.homeContent}>
 			<h1>
 				Order your exclusive <span>clock</span> with us today!
 			</h1>
@@ -20,7 +14,7 @@ function Home() {
 				<span> – William Shakespeare</span>
 			</div>
 			<Link to="/shop">VISIT THE SHOP</Link>
-		</motion.div>
+		</div>
 	);
 }
 
